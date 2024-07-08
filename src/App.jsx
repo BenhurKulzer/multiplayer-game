28415b082aea17bfe5c2c502c8e3dd7649eb8ff5
Benhur Külzer <benhur.kulzer@brandlovrs.com>
2024-07-08 09:40:27 -0300

    chore: adjust fov size according device orientation to improve usuability on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 import { Loader, PerformanceMonitor, SoftShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
@@ -10,6 +10,31 @@ import { Leaderboard } from "./components/Leaderboard";
 
 function App() {
   const [downgradedPerformance, setDowngradedPerformance] = useState(false);
+  const [fovSize, setFovSize] = useState(35);
+
+  useEffect(() => {
+    const updateFovBasedOnOrientationAndDeviceType = () => {
+      const isMobile = window.innerWidth < 768;
+      const isPortrait = window.innerHeight > window.innerWidth;
+
+      if (isMobile) {
+        if (isPortrait) {
+          setFovSize(70);
+        } else {
+          setFovSize(35);
+        }
+      } else {
+        setFovSize(35);
+      }
+    };
+
+    updateFovBasedOnOrientationAndDeviceType();
+    window.addEventListener('resize', updateFovBasedOnOrientationAndDeviceType);
+
+    return () => {
+      window.removeEventListener('resize', updateFovBasedOnOrientationAndDeviceType);
+    };
+  }, []);
 
   return (
     <>
@@ -19,7 +44,7 @@ function App() {
 
       <Canvas
         shadows
-        camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
+        camera={{ position: [0, 30, 0], fov: fovSize, near: 2 }}
         dpr={[1, 1.5]}
       >
         <color attach="background" args={["#242424"]} />
